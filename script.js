6e6ac9076d0c9cd84f6f347ce197f370b0dc5524
loganwe <91347950+loganwe@users.noreply.github.com>
2022-06-06 09:35:34 -0400

    initial commit

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ document.getElementById("button").onclick = function () {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
   if (username === Logan.user && password === Logan.password) {
-    document.write("<a href= 'https://lzw26l.csb.app/'>Click me</a>");
+    document.write(
+      "<link href='style.css' rel='stylesheet' /> <a href= 'https://lzw26l.csb.app/'>Click me</a>"
+    );
+  } else if (username === "edit" && password === "edit") {
+    document.write(
+      "<link href='style.css' rel='stylesheet' /> <a href='https://4nq6dh.csb.app/'>Click here</a>"
+    );
+  } else {
+    document.write(
+      "<title>Error</title> <link href='style2.css' rel='stylesheet' /> <br><br><br><p>ERROR</p>"
+    );
   }
 };
